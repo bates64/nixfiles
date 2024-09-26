876427095d863e8541b5e17c0217ac935f29e857
Alex Bates <alex.bates@arm.com>
2024-09-26 10:59:10 +0100

    fix kitty on non-NixOS by wrapping with nixGL

--- core.nix
+++ core.nix
@@ -1,6 +1,28 @@
 { lib, config, pkgs, ... }:
 
-{
+let
+  # https://github.com/nix-community/nixGL
+  useNixGL = !("system" ? config); # if not NixOS
+  wrapGL = { name, package }: # wrap a package's bins with nixGL
+    if useNixGL
+    then pkgs.runCommand "${name}-wrapped" {} ''
+      mkdir -p $out
+      cp -r ${package}/* $out
+      
+      # wrap every binary with nixGL
+      mv $out/bin $out/oldbin
+      mkdir $out/bin
+      for file in "$out/oldbin"/*; do
+        filename=$(basename "$file")
+        newfile="$out/bin/$filename"
+
+        echo "#!/bin/bash" > "$newfile"
+        echo "nixGL \"$file\" \"\$@\"" >> "$newfile"
+        chmod +x "$newfile"
+      done
+    ''
+    else package;
+in {
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "vscode" ];
 
   programs.zsh = {
@@ -16,9 +38,11 @@
     enable = true;
     settings = {
       enable_audio_bell = false;
-    
       window_padding_width = 16;
+      update_check_interval = 0;
     };
+    shellIntegration.enableZshIntegration = true;
+    package = wrapGL { name = "kitty"; package = pkgs.kitty; };
   };
 
   programs.vscode = {
