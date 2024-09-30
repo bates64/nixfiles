8f4d67cf39347021df751d84c45f723d89070ca6
Alex Bates <alex.bates@arm.com>
2024-09-30 14:20:11 +0100

    install bspwm

---
+++ hm/bspwm/default.nix
@@ -0,0 +1,39 @@
+{ pkgs, ...}:
+{
+  home.file.".xinitrc".source = ./xinitrc;
+
+  xsession.windowManager.bspwm = {
+    enable = true;
+    startupPrograms = [ "kitty" ];
+    extraConfig = ''
+      # mouse
+      bspc config pointer_action1 move
+      bspc config pointer_action2 resize_side
+      bspc config pointer_action2 resize_corner
+    '';
+  };
+
+  services.sxhkd = {
+    enable = true;
+    keybindings = {
+      "super + Return" = "kitty";
+      "super + alt + q" = "bspc quit";
+
+      # directional focus/swap window
+      "super + {_,shift + }{h,j,k,l}" = "bspc node -{f,s} {west,south,north,east}";
+
+      # focus/send desktop
+      "super + {_,shift + }{1-9,0}" = "bspc {desktop -f,node -d} '^{1-9,10}'";
+
+      # grow window
+      "super + alt + {h,j,k,l}" = "bspc node -z {left -20 0,bottom 0 20,top 0 -20,right 20 0}";
+
+      # shrink window
+      "super + alt + shift + {h,j,k,l}" = "bspc node -z {right -20 0,top 0 20,bottom 0 -20, left 20 0}";
+    };
+  };
+
+  home.packages = with pkgs; [ picom dunst feh ];
+
+  home.file.".config/gtk-3.0/settings.ini" = ./gtk-3.0-settings.ini;
+}
