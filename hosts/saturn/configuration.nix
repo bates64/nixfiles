4350376f8b8d4593a40134e538ffb54b68bbe036
Alex Bates <alex@bates64.com>
2024-11-10 16:03:39 +0000

    fix deprecated saturn config usage

--- hosts/saturn/configuration.nix
+++ hosts/saturn/configuration.nix
@@ -67,13 +67,12 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
+  services.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
-    layout = "us";
-    xkbVariant = "";
+    xkb.layout = "us";
   };
 
   # Configure console keymap
