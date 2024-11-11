1fbdb41604acaee29488ff48b4cf9617c4ba1c45
Alex Bates <alex@bates64.com>
2024-11-11 22:45:38 +0000

    remove factorio-headless mods

--- services/factorio.nix
+++ services/factorio.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, ... }: {
   services.factorio = {
     enable = true;
     nonBlockingSaving = true;
@@ -6,5 +6,17 @@
     allowedPlayers = [
       "bates64"
     ];
+    # https://forums.factorio.com/viewtopic.php?t=117096
+    package = pkgs.stdenv.mkDerivation {
+      name = "factorio-headless-no-space-age";
+      src = pkgs.factorio-headless.src;
+      buildPhase = ''
+        mkdir -p $out
+        cp -r ${pkgs.factorio-headless}/. $out
+        rm -rf $out/quality
+        rm -rf $out/elevated-rails
+        rm -rf $out/space-age
+      '';
+    };
   };
 }
