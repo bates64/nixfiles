c2346c6784c4fa2c9a167a6adde8bf4e822a0abf
Alex Bates <alex@bates64.com>
2024-11-18 17:34:30 +0000

    change apollo username to bates64

--- hosts/apollo/configuration.nix
+++ hosts/apollo/configuration.nix
@@ -63,7 +63,7 @@
   # services.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.alex = {
+  users.users.bates64 = {
      isNormalUser = true;
      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
      packages = with pkgs; [
