d96f697979104fbdeb3162d34edd7f556e917296
Alex Bates <alex@bates64.com>
2024-11-18 17:38:08 +0000

    add --no-write-lock-file to auto-upgrade

--- tasks/auto-upgrade.nix
+++ tasks/auto-upgrade.nix
@@ -6,6 +6,7 @@
       "--update-input" # deprecated, see https://github.com/NixOS/nixpkgs/issues/349734
       "nixpkgs"
       "-L" # print build logs
+      "--no-write-lock-file"
     ];
     allowReboot = true;
     rebootWindow = { lower = "03:00"; upper = "06:00"; };
