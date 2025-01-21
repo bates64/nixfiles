cdf133697db80195e2f08ec5328d273a3796d74a
Alex Bates <alex@bates64.com>
2025-01-21 05:29:03 +0000

    allow home-manager to install itself

--- home/bates64/cli/default.nix
+++ home/bates64/cli/default.nix
@@ -18,5 +18,6 @@
     home.username = lib.mkDefault "bates64";
     home.homeDirectory = lib.mkDefault (if config.isMacOS then "/Users/bates64" else "/home/bates64");
     home.stateVersion = lib.mkDefault "22.11";
+    programs.home-manager.enable = true;
   };
 }
