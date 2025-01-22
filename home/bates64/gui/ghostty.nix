08987ad9c10450aff5f3efe647f52b028d5f00e6
Alex Bates <alex.bates@arm.com>
2025-01-22 10:39:36 +0000

    add ghostty

--- home/bates64/gui/ghostty.nix
+++ home/bates64/gui/ghostty.nix
@@ -1,4 +1,26 @@
-{ pkgs, ... }:
-{
-  home.packages = with pkgs; [ ghostty ];
+{ config, ... }: {
+  programs.ghostty = {
+    enable = true;
+    installVimSyntax = true;
+
+    # shell integrations needed for nix develop
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+
+    settings = {
+      theme = "catppuccin-mocha";
+      font-size = 10;
+      background-opacity = if config.isMacOS then 0.4 else 1.0;
+      background-blur-radius = 20;
+      window-padding-x = 4;
+      window-padding-y = 4;
+      window-padding-balance = true;
+      window-padding-color = "extend";
+      window-inherit-working-directory = true;
+      clipboard-trim-trailing-spaces = true;
+      copy-on-select = false;
+      macos-titlebar-style = "tabs";
+      auto-update = "off";
+    };
+  };
 }
