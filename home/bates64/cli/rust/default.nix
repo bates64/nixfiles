b0482ef040d8b1b8b0f68f95368b8a029d448ca2
Alex Bates <alex@bates64.com>
2025-02-04 17:51:01 +0000

    rust: only get clang and mold when needed

--- home/bates64/cli/rust/default.nix
+++ home/bates64/cli/rust/default.nix
@@ -1,5 +1,5 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 {
-  home.packages = with pkgs; [ rustup mold-wrapped clang ];
+  home.packages = with pkgs; [ rustup ] ++ lib.optional (!config.isMacOS) [ clang mold-unwrapped ];
   home.file.".cargo/config.toml".source = ./config.toml;
 }
