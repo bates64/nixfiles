d0a05d7ca0ed401e78589971f7fd8f33a7941da0
Alex Bates <alex@bates64.com>
2025-02-05 22:00:26 +0000

    mold

--- home/bates64/cli/rust/default.nix
+++ home/bates64/cli/rust/default.nix
@@ -1,5 +1,5 @@
 { pkgs, lib, config, ... }:
 {
-  home.packages = with pkgs; [ rustup ] ++ lib.optional (!config.isMacOS) (with pkgs; [ clang mold-unwrapped ]);
+  home.packages = with pkgs; [ rustup ] ++ lib.optional (!config.isMacOS) (with pkgs; [ clang mold ]);
   home.file.".cargo/config.toml".source = ./config.toml;
 }
