55b2da6be4dbfa2fcb257df034521ef39537a2a6
Alex Bates <alex.bates@arm.com>
2025-02-07 16:43:48 +0000

    add sketchybar

--- home/bates64/gui/aerospace/default.nix
+++ home/bates64/gui/aerospace/default.nix
@@ -1,6 +1,10 @@
 { config, lib, pkgs, ... }: {
   config = lib.mkIf config.isMacOS {
-    home.packages = [ pkgs.aerospace ];
+    home.packages = [ pkgs.aerospace pkgs.sketchybar ];
     home.file.".config/aerospace/aerospace.toml".source = ./aerospace.toml;
+
+    home.file.".config/sketchybar/sketchybarrc" = { source = ./sketchybar/sketchybarrc; executable = true; };
+    #home.file.".config/sketchybar/plugins".source = ./sketchybar/plugins;
+    #home.file.".config/sketchybar/plugins".recursive = true;
   };
 }
