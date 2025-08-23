ccfff0eaa355324bf60c4ddc3bc6bd6c3338deb9
Alex Bates <alex@bates64.com>
2025-08-23 17:11:52 +0100

    aerospace: launch on login

--- home/bates64/gui/aerospace/default.nix
+++ home/bates64/gui/aerospace/default.nix
@@ -2,6 +2,16 @@
   config = lib.mkIf config.isMacOS {
     home.packages = [ pkgs.aerospace pkgs.sketchybar ];
     home.file.".config/aerospace/aerospace.toml".source = ./aerospace.toml;
+    launchd.agents.aerospace = {
+      enable = true;
+      config = {
+        Program = "${pkgs.aerospace}/Applications/AeroSpace.app/Contents/MacOS/AeroSpace";
+        KeepAlive = true;
+        RunAtLoad = true;
+        StandardOutPath = "/tmp/aerospace.log";
+        StandardErrorPath = "/tmp/aerospace.err.log";
+      };
+    };
 
     home.file.".config/sketchybar/sketchybarrc" = { source = ./sketchybar/sketchybarrc; executable = true; };
     #home.file.".config/sketchybar/plugins".source = ./sketchybar/plugins;
